3bc1fab6aac259bfb13ca1f38716489ccc0b0689
João Vieira <joaopedro12v@gmail.com>
2021-11-09 15:06:20 -0300

    conexão com a API

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -5,21 +5,26 @@ export const Home = () => {
         <div>
             <Container>
                 <div className="d-flex">
-                    <div className="m-auto p-2"><h1>Home</h1></div>  
-                </div>
-                <div className="p-2">
-                    <a href="/listar-cliente"
-                        className="btn btn-outline-success btn-sm">
-                        Cliente
-                    </a>
-                    <a href="/listar-pedido"
-                        className="btn btn-outline-success btn-sm">
-                        Pedido
-                    </a>
-                    <a href="/listar-servico"
-                        className="btn btn-outline-success btn-sm">
-                        Servico
-                    </a>
+                    <div className="m-auto p-2"><h1>Home</h1></div>
+
+                    <div className="p-2">
+                        <a href="/listar-cliente"
+                            className="btn btn-outline-success btn-sm">
+                            Cliente
+                        </a>
+                    </div>
+                    <div className="p-2">
+                        <a href="/listar-pedido"
+                            className="btn btn-outline-success btn-sm">
+                            Pedido
+                        </a>
+                    </div>
+                    <div className="p-2">
+                        <a href="/listar-servico"
+                            className="btn btn-outline-success btn-sm">
+                            Serviço
+                        </a>
+                    </div>
                 </div>
             </Container>
         </div>
--- src/views/Servico/Listar/index.js
+++ src/views/Servico/Listar/index.js
@@ -1,5 +1,70 @@
-export const ListarServ = () =>{
-    return(
-        <div>Listar Serviços</div>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container, Table } from "reactstrap";
+
+import { api } from "../../../config";
+
+export const ListarServ = () => {
+
+    const [data, setData] = useState([]);
+
+    const getServicos = async () => {
+        await axios.get(api + "/listaservicos")
+            .then((response) => {
+                console.log(response.data.servicos);
+                setData(response.data.servicos)
+            })
+            .catch(() => {
+                console.log("Sem conexão com a API")
+            });
+    };
+
+    useEffect(() => {
+        getServicos();
+    }, [])
+
+
+    return (
+        <div>
+            <Container>
+                <h1>Visualizar informações do serviço</h1>
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>
+                                ID
+                            </th>
+                            <th>
+                                Nome
+                            </th>
+                            <th>
+                                Descrição
+                            </th>
+                            <th>
+                                Ação
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(item => (
+                            <tr key={item.id}>
+                                <td>
+                                    {item.id}
+                                </td>
+                                <td>
+                                    {item.nome}
+                                </td>
+                                <td>
+                                    {item.descricao}
+                                </td>
+                                <td className="texte-center">
+                                    Button
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
     );
 };
